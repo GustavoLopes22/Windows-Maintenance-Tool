c12f2c7da04efe6eaefebed733adf0a54ef00c5f
Gustavo <gustavovalorx@gmail.com>
2026-01-30 11:08:52 -0300

    settings orientation

--- config.ps1
+++ config.ps1
@@ -1,8 +1,7 @@
-# true  = simulação / false = execução real
-$DRY_RUN = $false
+$DRY_RUN = $false #true = do not perform any changes, only simulate actions; false = perform changes.
 
-$DAYS_OLD_DOWNLOADS = 30
-$MOVE_INSTEAD_DELETE = $true
+$DAYS_OLD_DOWNLOADS = 15 #Number of days to consider a download "old".
+$MOVE_INSTEAD_DELETE = $false #true = move files to $OLD_FOLDER_NAME folder instead of deleting; false = delete files permanently.
 $OLD_FOLDER_NAME = "_OLD"
 
 $LOG_DIR = ".\logs"
